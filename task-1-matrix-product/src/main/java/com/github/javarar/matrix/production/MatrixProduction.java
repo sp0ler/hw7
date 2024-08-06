cc6c6984c29528971e4b9e16dbdcddd09593f6d6
denis <hdsp0ler@gmail.com>
2024-08-06 14:46:21 +0300

    home-work-v1

--- task-1-matrix-product/src/main/java/com/github/javarar/matrix/production/MatrixProduction.java
+++ task-1-matrix-product/src/main/java/com/github/javarar/matrix/production/MatrixProduction.java
@@ -1,13 +1,40 @@
 package com.github.javarar.matrix.production;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public class MatrixProduction {
 
-    public static Matrix product(Matrix a, Matrix b) {
-        throw new UnsupportedOperationException("реализуй меня");
-    }
+    public static int[][] product(
+            int rowA, int colA, int[][] a,
+            int rowB, int colB, int[][] b)
+    {
+        if (rowB != colA) {
+            throw new IllegalArgumentException("Невозможно посчитать произведение матриц!");
+        }
 
+        int[][] c = new int[rowA][colB];
+        List<CompletableFuture<Void>> futures = new ArrayList<>(a.length * b.length);
 
-    public static class Matrix {
-        // здесь можно реализовать все методы, которые необъодимы для работы с матрицами
+        for (int i = 0; i < rowA; i++) {
+            for (int j = 0; j < colB; j++) {
+                for (int k = 0; k < rowB; k++) {
+                    int finalI = i;
+                    int finalJ = j;
+                    int finalK = k;
+
+                    futures.add(
+                            CompletableFuture.runAsync(() -> {
+                                c[finalI][finalJ] += a[finalI][finalK] * b[finalK][finalJ];
+                            })
+                    );
+                }
+            }
+        }
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+        return c;
     }
 }
